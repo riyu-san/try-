93e1a87424e9ed741933902fd23fbceb489ebd40
riyu-san <virtudazoy@gmail.com>
2024-09-14 22:04:21 +0800

    version 2 (navbar hidden links responsive)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
-import Logo from '../assets/bakery-business.jpg'
-import { NavLink } from 'react-router-dom';
-import '../styles/Navbar.css';
+import Logo from '../assets/bakery-business.jpg';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
+  const location = useLocation();
 
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
 
+  useEffect(() => {
+    // Close the menu when the route changes
+    setOpenLinks(false);
+  }, [location]);
+
   return (
     <div className='navbar'>
-      <div className='leftSide' id={openLinks ? 'open' : 'close'}>
+      <div className={`overlay ${openLinks ? 'active' : ''}`} onClick={toggleNavbar}></div>
+      <div className='leftSide'>
         <NavLink to='/'>
-            <img src={Logo} alt="Logo" />
+          <img src={Logo} alt="Logo" />
         </NavLink>
-        <div className='hiddenLinks'>
+        <div className={`hiddenLinks ${openLinks ? 'open' : ''}`}>
           <NavLink to='/' exact activeClassName='active'>Home</NavLink>
           <NavLink to='/menu' activeClassName='active'>Menu</NavLink>
           <NavLink to='/about' activeClassName='active'>About</NavLink>
@@ -35,7 +41,7 @@ function Navbar() {
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
